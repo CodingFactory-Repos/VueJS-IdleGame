df21b75bb59fd1606a23f0be7d95a702b4f3b449
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2023-10-04 14:17:05 +0200

    Tailwind

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,19 +1,26 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer />
       <h1 class="bg-blue-600 text-9xl">Hello</h1>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
 </script>
 
 <style scoped>
 ion-content {
-  --background: #ffffff url('../../public/images/bg.png') no-repeat center center / cover;
+  --background: #ffffff url("../../public/images/bg.png") no-repeat center
+    center / cover;
 }
 </style>
