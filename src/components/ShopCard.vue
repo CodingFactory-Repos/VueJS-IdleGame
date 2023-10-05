5d692513535bc8a6c3a952a57fc38d2fa0eb007a
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2023-10-05 10:28:39 +0200

    Add new shop design

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -1,60 +1,60 @@
 <template>
-    <ion-card>
+  <ion-card class="flex flex-col items-center justify-center">
+    <img
+        v-bind:src="'../../public/images/miners/' + product?.image"
+        alt="product image"
+    />
+    <h5
+        class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+    >
+      {{ product?.name }}
+    </h5>
+    <p class="mt-2 text-gray-600 dark:text-gray-400 text-sm text-center mx-2">
+      La machine {{ product?.name }} vous permet de miner {{ product?.generate_per_seconds_in_crypto.toFixed(5) }}
+      {{ product?.eur_to }} (Soit {{ product?.generate_per_seconds }} € par seconde). Elle coûte {{ product?.price }} €.
+      Pour la rentabiliser, il vous faudra environ
+      {{ (((product?.price / product?.generate_per_seconds)) / 60).toFixed(0) }} minutes.
+    </p>
+    <div class="counter">
+      <span class="text-gray-700 flex-row flex">
+        {{ product?.price_in_crypto.toFixed(5) }}
         <img
-            class="p-8 rounded-t-lg"
-            src="../../public/images/miners/el_monstro.gif"
-            alt="product image"
+            v-bind:src="'https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/svg/icon/' + product?.eur_to.toLowerCase() + '.svg'"
+            class="w-5 h-5 mx-1"/>
+        <span class="text-gray-500 text-xs">/ Soit {{ product?.price }} €</span>
+      </span>
+    </div>
+    <button
+        type="button"
+        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        @click="buyItem(product?._id)"
+    >
+      <svg
+          class="w-3.5 h-3.5 mr-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 21"
+      >
+        <path
+            d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175 .745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"
         />
-        <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-        >
-            {{ product?.name }}
-        </h5>
-        <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-        >
-            {{ product?.name }}
-        </h5>
-        <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-        >
-            {{ product?.name }}
-        </h5>
-        <div class="flex items-center justify-between">
-            <div class="counter">
-                <span>{{ product?.price }} crédits</span>
-            </div>
-            <button
-                type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center w-full mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                @click="buyItem(product?._id)"
-            >
-                <svg
-                    class="w-3.5 h-3.5 mr-2"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 21"
-                >
-                    <path
-                        d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175 .745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"
-                    />
-                </svg>
-                Buy now
-            </button>
-        </div>
-    </ion-card>
+      </svg>
+      Buy now
+    </button>
+  </ion-card>
 </template>
 
 <script setup lang="ts">
-import { useShopStore } from "@/stores/shop";
+import {useShopStore} from "@/stores/shop";
+
 const shopStore = useShopStore();
 
 defineProps({
-    product: Object,
+  product: Object,
 });
 
 const buyItem = (_id: string) => {
-    shopStore.buyProduct(_id);
+  shopStore.buyProduct(_id);
 };
 </script>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -12,7 +12,7 @@
                 </ion-toolbar>
             </ion-header>
 
-            <div class="grid grid-cols-2">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <div v-if="!shopStore.isLoading">
                     <ShopCard
                         v-for="(product, index) in shop"
@@ -53,93 +53,6 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-/* Style pour la mise en page de chaque carte */
-.card-content {
-    display: flex;
-    flex-direction: column; /* Pour que le texte apparaisse sous l'image sur les appareils mobiles */
-}
-
-ion-card {
-    background: #01062e; /* Remplacez cette couleur par celle de votre choix */
-}
-
-.card-image {
-    text-align: center;
-    padding-top: 1px;
-}
-
-.card-image img {
-    max-width: 100%;
-    height: auto;
-    cursor: pointer; /* Curseur de type main pointer lorsqu'on survole l'image */
-}
-
-/* Styles pour les informations de l'usine en haut à droite */
-
-.info-top-right {
-    display: flex;
-    justify-content: space-between; /* Alignement des informations à droite */
-    align-items: center; /* Alignement vertical */
-    /* margin-top: 10px; */
-}
-
-/* Style pour le compteur et les boutons */
-.counter {
-    display: flex;
-    align-items: center;
-}
-
-.counter ion-button {
-    /* background-color: #3498db; */
-    color: #fff;
-    border: none;
-    font-size: 15px;
-    padding: 2px 4px;
-    cursor: pointer;
-}
-
-.counter span {
-    font-size: 18px;
-    margin: 0 10px;
-}
-
-/* Style pour le bouton "Acheter" */
-ion-button[expand="full"] {
-    margin-top: 10px;
-    color: #fff;
-    border: none;
-    font-size: 20px;
-    padding: 10px;
-}
-
-.image-info {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    /* margin-top: 2px; */
-}
-
-.image-info p {
-    font-size: 16px;
-    margin: 0;
-}
-
-.card-grid {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr); /* Trois colonnes égales par ligne */
-    grid-gap: 10px; /* Espacement entre les cartes */
-}
-
-/* Assurez-vous que les cartes ne dépassent pas la largeur de leur conteneur */
-ion-card {
-    max-width: 100%;
-}
-
-@media (max-width: 600px) {
-    .card-grid {
-        grid-template-columns: repeat(1, 1fr); /* Une colonne par ligne */
-    }
-}
 </style>
 
 <!-- <img src="../../public/images/miners/el_monstro.gif" :alt="'Image de l\'usine '" /> -->
