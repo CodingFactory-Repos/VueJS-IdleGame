71c73dbad202c9e23024286c0a61921c26bf0680
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2023-10-05 23:40:51 +0200

    Update design

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -2,17 +2,23 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Marketplace</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Marketplace</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 3 page" />
+      <div>
+        <ShopCard
+              v-for="(product, index) in shop"
+              :key="index"
+              :product="product"
+          />
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -20,4 +26,7 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import ShopCard from "@/components/ShopCard.vue";
+
+
 </script>
