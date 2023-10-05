3114c5ecab07d02c2fca553e4083ea9db864abb2
Ibrahima2847 <ibuseck670@gmail.com>
2023-10-05 16:05:38 +0200

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -18,13 +18,14 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" placeholder="example@example.com" />
+                        <label for="email">Username:</label>
+                        <input type="text" id="email" placeholder="example@example.com" v-model="email" />
                     </div>
                     <div class="form-group">
                         <label for="password">Password:</label>
-                        <input type="password" id="password" placeholder="Password" />
+                        <input type="password" id="password" placeholder="Password" v-model="password" />
                     </div>
+
                     <button class="btn-login" type="submit">Login</button>
                 </form>
             </div>
@@ -37,6 +38,7 @@ import {
     IonPage,
     IonContent,
 } from "@ionic/vue";
+import axios from 'axios';
 
 export default {
     data() {
@@ -47,17 +49,58 @@ export default {
         };
     },
     methods: {
-        login() {
+        async login() {
+            const userEmail = this.email;
+            const userPassword = this.password;
 
-            if (this.email === "is@gmail.com" && this.password === "a") {
-                console.log("Reuissi");
-                this.$router.push('/tabs/tab1');
-            } else {
-                this.errorMessage = 'Identifiants incorrects';
-                this.email = '';
-                this.password = '';
+            try {
+                const response = await axios.post('http://localhost:3001/auth/login', {
+                    username: userEmail,
+                    password: userPassword
+                });
+
+                if (response.status === 200) {
+                    // Authentification réussie, vous pouvez rediriger l'utilisateur ou effectuer d'autres actions ici
+                    const responseData = response.data;
+                    const token = responseData.token;
+
+                    // Effectuer une nouvelle requête pour récupérer les informations de l'utilisateur
+                    const userResponse = await axios.get('http://localhost:3001/auth/me', {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    });
+
+                    if (userResponse.status === 200) {
+                        // Informations de l'utilisateur récupérées avec succès
+                        const user = userResponse.data;
+                        console.log('User:', user);
+
+                        // Comparez les données de l'utilisateur, par exemple, le nom d'utilisateur ou d'autres informations
+                        if (user && user.username === this.email) {
+                            // L'utilisateur est correct, vous pouvez stocker le jeton ou effectuer d'autres actions
+                            this.$router.push('/tabs/tab1');
+                            localStorage.setItem("monToken", token);
+                        } else {
+                            // L'utilisateur n'est pas correct, affichez un message d'erreur
+                            this.errorMessage = 'Identifiants incorrects';
+
+                        }
+                    } else {
+                        // Erreur lors de la récupération des informations de l'utilisateur
+                        this.errorMessage = 'Identifiants incorrects';
+
+                    }
+                } else {
+                    // Afficher un message d'erreur en cas d'échec de l'authentification
+                    this.errorMessage = 'Identifiants incorrects';
+
+                }
+
+            } catch (error) {
+
+                console.error(error);
             }
-
         }
     }
 };
@@ -88,7 +131,6 @@ ion-content {
 h2 {
     margin-bottom: 20px;
 }
-
 .ion-content-center {
     display: flex;
     justify-content: center;
@@ -119,7 +161,7 @@ label {
     font-weight: bold;
 }
 
-input[type="email"],
+input[type="text"],
 input[type="password"] {
     width: 100%;
     padding: 10px;
@@ -138,5 +180,6 @@ input[type="password"] {
 
 .btn-login:hover {
     background-color: #0055b3;
-}</style>
+}
+</style>
   
