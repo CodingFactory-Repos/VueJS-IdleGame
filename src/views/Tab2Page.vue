5436023f3ef1e4c1f5eea7dfaa75929496f3cac4
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2023-10-05 14:34:44 +0200

    Update header

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,54 +1,58 @@
 <template>
-    <ion-page>
-        <ion-header>
-            <ion-toolbar>
-                <ion-title>Shop</ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content :fullscreen="true">
-            <ion-header collapse="condense">
-                <ion-toolbar>
-                    <ion-title size="large">Shop</ion-title>
-                </ion-toolbar>
-            </ion-header>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Shop</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Shop</ion-title>
+        </ion-toolbar>
+      </ion-header>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                <div v-if="!shopStore.isLoading">
-                    <ShopCard
-                        v-for="(product, index) in shop"
-                        :key="index"
-                        :product="product"
-                    />
-                </div>
-                <div v-else>
-                    <p>No shop store</p>
-                </div>
-            </div>
-            <!-- </div> -->
-        </ion-content>
-    </ion-page>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <ion-card class="flex flex-col items-center justify-center">
+          <h3 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            Vous avez actuellement {{ user?.money.toFixed(5) }} BTC.
+          </h3>
+        </ion-card>
+        <div v-if="!shopStore.isLoading">
+          <ShopCard
+              v-for="(product, index) in shop"
+              :key="index"
+              :product="product"
+          />
+        </div>
+        <div v-else>
+          <p>No shop store</p>
+        </div>
+      </div>
+      <!-- </div> -->
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import {onMounted, ref} from "vue";
 import ShopCard from "@/components/ShopCard.vue";
 
-import {
-    IonPage,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-} from "@ionic/vue";
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar,} from "@ionic/vue";
 
-import { useShopStore, Product } from "../stores/shop";
+import {Product, useShopStore} from "@/stores/shop";
+import {User, useUserStore} from "@/stores/user";
+
 const shopStore = useShopStore();
+const userStore = useUserStore();
 
 const shop = ref<Product[]>([]);
+const user = ref<User>();
 
 onMounted(async () => {
-    const products = await shopStore.fetchProducts();
-    shop.value = products;
+  shop.value = await shopStore.fetchProducts();
+
+  user.value = await userStore.fetchUser();
 });
 </script>
 
