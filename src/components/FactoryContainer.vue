01413279705b5215ce1dcc9c164291bf6f2230b3
Rui Gaspar <ruigaspar03@hotmail.com>
2023-10-06 11:25:13 +0200

    Add upgrade and get item farm

--- src/components/FactoryContainer.vue
+++ src/components/FactoryContainer.vue
@@ -11,8 +11,14 @@
             <!-- Render the inventory items here -->
             <div v-for="item in inventoryStore.getItems" :key="item._id">
                 <ion-card class="rounded-lg shadow-md p-4">
+                    <span
+                        class="font-bold text-xl flex items-center justify-center"
+                    >
+                        {{ item.shop_data.name }}
+                    </span>
                     <ion-card-content class="flex items-center">
                         <!-- Image de l'usine avec cadre foncé -->
+
                         <div class="relative w-1/3 h-1/3 mr-8">
                             <img
                                 :src="`../../public/images/miners/${item.shop_data.image}`"
@@ -23,22 +29,51 @@
                                 class="absolute inset-0 bg-black opacity-30 rounded-lg"
                             ></div>
                         </div>
-                        <div class="flex-1">
-                            <h1 class="text-2xl font-bold">
-                                {{ item.shop_data.name }}
-                            </h1>
-                            <!-- Texte "price: 1" -->
-                            <p class="ml-2 text-xl">
-                                Votre GPU vous rapporte
-                                {{
-                                    (
-                                        item.shop_data.generate_per_seconds +
-                                        (item.inventory_data.level - 1) *
-                                            0.1 *
-                                            item.shop_data.generate_per_seconds
-                                    ).toFixed(2)
-                                }}
-                                €/s
+                        <div class="flex-1 gap-1 flex flex-col">
+                            <p class="text-xs">
+                                Rapport:
+                                <span class="font-bold text-xs">
+                                    {{
+                                        (
+                                            item.shop_data
+                                                .generate_per_seconds +
+                                            (item.inventory_data.level - 1) *
+                                                0.1 *
+                                                item.shop_data
+                                                    .generate_per_seconds
+                                        ).toFixed(2)
+                                    }}
+                                    €/s
+                                </span>
+                            </p>
+                            <p class="text-xs">
+                                Level Up Price:
+                                <span class="font-bold">
+                                    {{ item.inventory_data.upgrade_price }}
+                                    BTC
+                                </span>
+                            </p>
+                            <p class="text-xs">
+                                Last Collect:
+                                <span class="font-bold">
+                                    {{
+                                        // calculate the time left until the next collect
+                                        // last_reward = 2023-10-06T08:21:20.177+00:00
+                                        // now = 2023-10-06T08:21:20.177+00:00
+                                        // next_collect = 2023-10-06T08:21:20.177+00:00 + 30min
+
+                                        moment(item.inventory_data.last_reward)
+                                            .add(
+                                                new Date(
+                                                    item.shop_data
+                                                        .generate_per_seconds *
+                                                        1000
+                                                ).getMinutes(),
+                                                "seconds"
+                                            )
+                                            .fromNow()
+                                    }}
+                                </span>
                             </p>
                         </div>
                     </ion-card-content>
@@ -47,6 +82,12 @@
                         <button
                             type="button"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center justify-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            @click="
+                                onClaimReward(
+                                    item.inventory_data.item_id,
+                                    item.inventory_data.row_id
+                                )
+                            "
                         >
                             Collecter les gains
                         </button>
@@ -54,7 +95,7 @@
                             type="button"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center justify-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                             @click="
-                                inventoryStore.levelUpItem(
+                                onLevelUp(
                                     item.inventory_data.item_id,
                                     item.inventory_data.row_id
                                 )
@@ -85,14 +126,14 @@
                             Sell Miner
                         </button>
                     </div>
-                    <div class="flex items-center justify-center mt-4">
-                        <div class="relative w-3/4 h-2 bg-gray-200">
+                    <!-- <div class="flex items-center justify-center mt-4">
+                        <div class="relative w-3/4 h-2 bg-gray-200 rounded-lg">
                             <div
-                                class="absolute h-full bg-teal-500 transition-width duration-500"
+                                class="absolute h-full bg-teal-500 transition-width duration-500 rounded-lg"
                                 :style="{ width: loadingPercentage + '%' }"
                             ></div>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="flex items-center justify-center mt-4"></div>
                 </ion-card>
             </div>
@@ -102,10 +143,13 @@
 
 <script setup lang="ts">
 import { useInventoryStore } from "@/stores/inventory";
+import { useUserStore } from "@/stores/user";
 import { IonAlert } from "@ionic/vue";
 import { onMounted, ref } from "vue";
+import moment from "moment";
 
 const inventoryStore = useInventoryStore();
+const userStore = useUserStore();
 
 const alertInputs = [
     {
@@ -145,22 +189,33 @@ const showSellAlert = () => {
 
 const loadingPercentage = ref(0);
 
-const startProgressBar = () => {
-    loadingPercentage.value = 0;
-    let timer = setInterval(() => {
-        loadingPercentage.value += (1000 / 50000) * 100;
-        if (loadingPercentage.value >= 100) {
-            clearInterval(timer);
-            setTimeout(() => {
-                loadingPercentage.value = 0;
-                startProgressBar();
-            }, 1000);
-        }
-    }, 100);
+// const startProgressBar = () => {
+//     loadingPercentage.value = 0;
+//     let timer = setInterval(() => {
+//         loadingPercentage.value += (1000 / 50000) * 100;
+//         if (loadingPercentage.value >= 100) {
+//             clearInterval(timer);
+//             setTimeout(() => {
+//                 loadingPercentage.value = 0;
+//                 startProgressBar();
+//             }, 1000);
+//         }
+//     }, 100);
+// };
+
+const onClaimReward = async (item_id: string, row_id: string) => {
+    await inventoryStore.claimReward(item_id, row_id);
+    await userStore.fetchUser();
+};
+
+const onLevelUp = async (item_id: string, row_id: string) => {
+    await inventoryStore.levelUpItem(item_id, row_id);
+    await userStore.fetchUser();
 };
 
 onMounted(async () => {
-    startProgressBar();
+    // startProgressBar();
+
     try {
         await inventoryStore.fetchInventory();
         console.log(inventoryStore.getItems);
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,20 +1,58 @@
 <template>
     <ion-page>
         <ion-content :fullscreen="true">
+            <ion-card class="flex flex-col items-center justify-center">
+                <h3
+                    class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white text-center w-full"
+                >
+                    <div class="flex items-center justify-center mb-4">
+                        <div class="relative w-3/4 h-2 bg-gray-200 rounded-lg">
+                            <div
+                                class="absolute h-full bg-blue-500 transition-width duration-500 rounded-lg"
+                                :style="{
+                                    width:
+                                        (userStore.getUser?.xp /
+                                            userStore.getUser
+                                                ?.xp_to_next_level) *
+                                            100 +
+                                        '%',
+                                }"
+                            ></div>
+                        </div>
+                    </div>
+                    Level :
+                    {{ userStore.getUser?.level }}
+                    <br />
+                    Slots :
+                    {{ userStore.getUser?.used_slots }} /
+                    {{ userStore.getUser?.slots_number }}
+                </h3>
+            </ion-card>
+            <ion-card class="flex flex-col items-center justify-center">
+                <h3
+                    class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white text-center"
+                >
+                    Vous avez actuellement :
+                    <br />
+                    {{ userStore.getUser?.money.toFixed(8) }} BTC
+                </h3>
+            </ion-card>
             <FactoryContainer />
         </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
-import {
-    IonPage,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-} from "@ionic/vue";
+import { IonPage, IonContent } from "@ionic/vue";
 import FactoryContainer from "@/components/FactoryContainer.vue";
+import { useUserStore } from "@/stores/user";
+import { onMounted } from "vue";
+
+const userStore = useUserStore();
+
+onMounted(async () => {
+    await userStore.fetchUser();
+});
 </script>
 
 <style scoped>
